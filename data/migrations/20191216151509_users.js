8cc1c7ee7fce410e2d1e58c86982c4a07c5aa1b6
Andrew Wilson <andrew.wilson90@hotmail.com>
2019-12-21 23:10:23 -0600

    createTables for roles users and trips

--- data/migrations/20191216151509_users.js
+++ data/migrations/20191216151509_users.js
@@ -1,16 +1,87 @@
 exports.up = function(knex) {
-  return knex.schema.createTable("users", tbl => {
-    tbl.increments();
+  return knex.schema
+    .createTable("roles", tbl => {
+      tbl.increments();
 
-    tbl
-      .string("username", 255)
-      .notNullable()
-      .unique();
+      tbl
+        .string("role")
+        .notNullable()
+        .unique();
+    })
+    .createTable("users", tbl => {
+      tbl.increments();
 
-    tbl.string("password", 8000).notNullable();
-  });
+      tbl
+        .string("username", 255)
+        .notNullable()
+        .unique();
+
+      tbl.string("password", 8000).notNullable();
+
+      tbl.string("fullname", 255).notNullable();
+
+      tbl.string("phone", 255).notNullable();
+
+      tbl.string("address", 255).notNullable();
+
+      tbl.string("state", 255).notNullable();
+
+      tbl.integer("zip").notNullable();
+
+      tbl.date("created").notNullable();
+
+      tbl
+        .integer("role_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("recipes")
+        .onDelete("RESTRICT")
+        .onUpdate("CASCADE");
+    })
+    .createTable("trips", tbl => {
+      tbl.increments();
+
+      tbl.date("date").notNullable();
+
+      tbl.string("airport", 255).notNullable();
+
+      tbl.string("flight", 255).notNullable();
+
+      tbl.time("departure").notNullable();
+
+      tbl.string("carryOn", 255).notNullable();
+
+      tbl.string("checked", 255).notNullable();
+
+      tbl.string("agentReq", 255);
+
+      tbl.string("special", 255);
+
+      tbl.string("upgrades", 255);
+
+      tbl
+        .integer("family_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("users")
+        .onDelete("RESTRICT")
+        .onUpdate("CASCADE");
+
+      tbl
+        .integer("agent_id")
+        .unsigned()
+        .references("id")
+        .inTable("users")
+        .onDelete("RESTRICT")
+        .onUpdate("CASCADE");
+    });
 };
 
 exports.down = function(knex) {
-  return knex.schema.dropTableIfExists("users");
+  return knex.schema
+    .dropTableIfExists("roles")
+    .dropTableIfExists("users")
+    .dropTableIfExists("trips");
 };
